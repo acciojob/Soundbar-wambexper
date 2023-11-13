6c2a2d217964be6ba218c8b5f70fe4e9b8790ee9
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-13 11:50:46 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let audioPlayer = document.getElementById('audioPlayer');
+let currentAudio = null;
+
+function playAudio(soundFileName) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+    audioPlayer.src = `sounds/${soundFileName}`;;
+    audioPlayer.play(); 
+    currentAudio = audioPlayer;
+    
+}
+
+function stopAudio() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+}
